8874a3ff4e97ddd239dad672bab3f634f58b9405
al8n <scygliu1@gmail.com>
2022-08-18 11:19:53 +0800

    add encryption

--- vpb/src/lib.rs
+++ vpb/src/lib.rs
@@ -8,7 +8,180 @@ mod ty;
 pub use ty::{kv::Kind, manifest_change::Operation, *};
 
 pub use prost;
+
+/// Encryption/Decryption
+#[cfg(feature = "encryption")]
+pub mod encrypt {
+    use aes::cipher::{KeyIvInit, StreamCipher};
+    use aes::{Aes128, Aes192, Aes256};
+
+    pub const BLOCK_SIZE: usize = 16;
+
+    /// AES-128 in CTR mode
+    pub type Aes128Ctr = ctr::Ctr64BE<Aes128>;
+
+    /// AES-192 in CTR mode
+    pub type Aes192Ctr = ctr::Ctr64BE<Aes192>;
+
+    /// AES-256 in CTR mode
+    pub type Aes256Ctr = ctr::Ctr64BE<Aes256>;
+
+    #[derive(Debug, Copy, Clone)]
+    pub enum EncryptError {
+        InvalidLength,
+        LengthMismatch { src: usize, dst: usize },
+        KeySizeError(usize),
+        // #[cfg(feature = "std")]
+        // IO(std::io::ErrorKind),
+    }
+
+    impl core::fmt::Display for EncryptError {
+        fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+            match self {
+                EncryptError::InvalidLength => write!(f, "aes: invalid length when generate AES",),
+                EncryptError::LengthMismatch { src, dst } => write!(
+                    f,
+                    "aes length mismatch: the length of source is {} and the length of destination {}",
+                    src, dst
+                ),
+                EncryptError::KeySizeError(sz) => write!(f, "aes: invalid key size {}", sz),
+                // EncryptError::IO(kd) => write!(f, "aes fail to write encrypt stream: {:?}", kd),
+            }
+        }
+    }
+
+    #[cfg(feature = "std")]
+    impl std::error::Error for EncryptError {}
+
+    /// AES encryption extensions
+    pub trait EncryptExt: AsMut<[u8]> + AsRef<[u8]> {
+        /// Encrypts self with IV.
+        /// Can be used for both encryption and decryption. IV is of
+        /// AES block size.
+        fn encrypt(&mut self, key: &[u8], iv: &[u8]) -> Result<(), EncryptError> {
+            let data = self.as_mut();
+            encrypt_in(data, key, iv)
+        }
+
+        /// Encrypts self with IV to a new `Vec`.
+        /// Can be used for both encryption and decryption. IV is of
+        /// AES block size.
+        fn encrypt_to_vec(&self, key: &[u8], iv: &[u8]) -> Result<Vec<u8>, EncryptError> {
+            let src = self.as_ref();
+            encrypt_to_vec(src, key, iv)
+        }
+
+        /// Encrypts self with IV to `dst`.
+        /// Can be used for both encryption and decryption. IV is of
+        /// AES block size.
+        fn encrypt_to(&self, dst: &mut [u8], key: &[u8], iv: &[u8]) -> Result<(), EncryptError> {
+            let src = self.as_ref();
+            encrypt_to(dst, src, key, iv)
+        }
+    }
+
+    impl<T: AsMut<[u8]> + AsRef<[u8]>> EncryptExt for T {}
+
+    /// Encrypts data with IV.
+    /// Can be used for both encryption and decryption. IV is of
+    /// AES block size.
+    #[inline]
+    pub fn encrypt(data: &mut [u8], key: &[u8], iv: &[u8]) -> Result<(), EncryptError> {
+        encrypt_in(data, key, iv)
+    }
+
+    /// Encrypts src with IV to a new `Vec`.
+    /// Can be used for both encryption and decryption. IV is of
+    /// AES block size.
+    #[inline]
+    pub fn encrypt_to_vec(src: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, EncryptError> {
+        let mut dst = src.to_vec();
+        encrypt_in(dst.as_mut(), key, iv).map(|_| dst)
+    }
+
+    /// Encrypts `src` with IV to `dst`.
+    /// Can be used for both encryption and decryption. IV is of
+    /// AES block size.
+    #[inline]
+    pub fn encrypt_to(
+        dst: &mut [u8],
+        src: &[u8],
+        key: &[u8],
+        iv: &[u8],
+    ) -> Result<(), EncryptError> {
+        if dst.len() != src.len() {
+            return Err(EncryptError::LengthMismatch {
+                src: src.len(),
+                dst: dst.len(),
+            });
+        }
+        dst.copy_from_slice(src);
+        encrypt_in(dst, key, iv)
+    }
+
+    #[inline(always)]
+    fn encrypt_in(dst: &mut [u8], key: &[u8], iv: &[u8]) -> Result<(), EncryptError> {
+        let kl = key.len();
+        match kl {
+            16 => Aes128Ctr::new_from_slices(key, iv)
+                .map_err(|_| EncryptError::InvalidLength)?
+                .apply_keystream(dst),
+            24 => Aes192Ctr::new_from_slices(key, iv)
+                .map_err(|_| EncryptError::InvalidLength)?
+                .apply_keystream(dst),
+            32 => Aes256Ctr::new_from_slices(key, iv)
+                .map_err(|_| EncryptError::InvalidLength)?
+                .apply_keystream(dst),
+            _ => return Err(EncryptError::KeySizeError(kl)),
+        }
+        Ok(())
+    }
+
+    /// generates IV.
+    #[inline]
+    pub fn random_iv() -> [u8; BLOCK_SIZE] {
+        #[cfg(feature = "encryption-std")]
+        {
+            use rand::{thread_rng, Rng};
+            let mut rng = thread_rng();
+            rng.gen::<[u8; BLOCK_SIZE]>()
+        }
+
+        #[cfg(not(feature = "encryption-std"))]
+        {
+            use rand::{rngs::OsRng, RngCore};
+            let mut key = [0u8; BLOCK_SIZE];
+            OsRng.fill_bytes(&mut key);
+            key
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+        use rand::{thread_rng, Rng};
+
+        #[test]
+        fn test_aes_block() {
+            let mut rng = thread_rng();
+            let key = rng.gen::<[u8; 32]>();
+            let iv = random_iv();
+
+            let mut src = [0u8; 1024];
+            rng.fill(&mut src);
+
+            let mut dst = vec![0u8; 1024];
+            encrypt_to(dst.as_mut_slice(), &src, &key, &iv).unwrap();
+
+            let act = encrypt_to_vec(dst.as_slice(), &key, &iv).unwrap();
+            assert_eq!(src.clone().to_vec(), act);
+        }
+    }
+}
+
+/// Checksum calculation.
 pub mod checksum {
+    /// Calculates checksum for data using ct checksum type.
     #[inline]
     pub fn calculate_checksum(data: &[u8], algorithm: super::ChecksumAlgorithm) -> u64 {
         match algorithm {
@@ -23,12 +196,35 @@ pub mod checksum {
         }
     }
 
+    /// Validates the checksum for the data against the given expected checksum.
+    #[inline]
+    pub fn verify_checksum(
+        data: &[u8],
+        expected: super::Checksum,
+    ) -> Result<bool, super::InvalidChecksumAlgorithm> {
+        super::ChecksumAlgorithm::try_from(expected.algo)
+            .map(|algo| calculate_checksum(data, algo) == expected.sum)
+    }
+
+    /// Validates the checksum for the data against the given expected checksum.
+    ///
+    /// # Panic
+    /// The algorithm is not one of the supported checksum algorithms.
+    #[inline]
+    pub fn verify_checksum_unchecked(data: &[u8], expected: super::Checksum) -> bool {
+        let algo =
+            super::ChecksumAlgorithm::try_from(expected.algo).expect("Invalid checksum algorithm");
+        calculate_checksum(data, algo) == expected.sum
+    }
+
+    /// Calculate crc32 checksum
     #[cfg(any(feature = "crc32", feature = "crc32-std"))]
     #[inline]
     pub fn crc32(data: &[u8]) -> u64 {
         crc32fast::hash(data) as u64
     }
 
+    /// Calculate sea hash checksum
     #[cfg(any(feature = "sea", feature = "sea-std"))]
     #[inline]
     pub fn sea(data: &[u8]) -> u64 {
@@ -45,6 +241,7 @@ pub mod checksum {
         h.finish()
     }
 
+    /// Calculate xxhash64 checksum
     #[cfg(any(feature = "xxhash64", feature = "xxhash64-std"))]
     #[inline]
     pub fn xxhash64(data: &[u8]) -> u64 {
